03928170b040c69f85aea2f394c91aa84f5f3cb7
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-12-16 16:16:50 +1000

    16-12-21 (2/2)
    - Add Index Creation
    - Implement Index to `OnProjectLoad`
    - Implement Index to `OnProjectSave`

--- NPC Register/NPC Register/DataManager.cs
+++ NPC Register/NPC Register/DataManager.cs
@@ -12,13 +12,26 @@ namespace NPC_Register
         SQLiteCommand cmd;
 
         public DataSet data;
+
+        //Dictionary Mapping
+        public Dictionary<string, int> tableIndex;
+        public Dictionary<string, int>[] rowIndexes;
+
+
         public bool debugging;
 
-        public DataManager (string connectionPath)
+        public DataManager (string connectionPath, bool debugging)
         {
+            //Intialise Variables
+            this.debugging = debugging;
+
             connection = new SQLiteConnection(connectionPath);
             cmd = connection.CreateCommand();
-            debugging = true;
+
+            tableIndex = GetTableIndex();
+            rowIndexes = GetRowIndexes();
+
+            //Begin Managing
             OnProjectLoad();
         }
 
@@ -33,10 +46,59 @@ namespace NPC_Register
             }
         }
 
+        #region Index Creation
+
+        Dictionary<string, int> GetTableIndex()
+        {
+            var tblIndex = new Dictionary<string, int>();
+            for (int i = 0; i < data.Tables.Count; i++)
+            {
+                tblIndex.Add(data.Tables[i].TableName, i);
+            }
+            return tblIndex;
+        }
+
+        Dictionary<string, int>[] GetRowIndexes()
+        {
+            var rwIndexes = new Dictionary<string, int>[data.Tables.Count];
+            for (int i = 0; i < data.Tables.Count; i++)
+            {
+                rwIndexes[i] = GetRowIndex(i);
+            }
+            return rwIndexes;
+        }
+
+        Dictionary<string, int> GetRowIndex(int tblIndex)
+        {
+            var rwIndex = new Dictionary<string, int>();
+            // If a Tables PK is not found in the first column, create a new `if`
+            // statement. Like CatalogueEntries.
+            if (data.Tables[tblIndex].TableName == "CatalogueEntries")
+            {
+                for (int i = 0; i < data.Tables[tblIndex].Rows.Count; i++)
+                {
+                    // Since this tables PK is two attributes, it is combinted
+                    // then seperated by a comma
+                    rwIndex.Add(data.Tables[tblIndex].Rows[i].ItemArray[0] + ", "
+                        + data.Tables[tblIndex].Rows[i].ItemArray[1], i);
+                }
+            }
+            //If a Table PK is found within the first column.
+            else
+            {
+                for (int i = 0; i < data.Tables[tblIndex].Rows.Count; i++)
+                {
+                    rwIndex.Add(data.Tables[tblIndex].Rows[i].ItemArray[0].ToString(), i);
+                }
+            }
+            return rwIndex;
+        }
+
+        #endregion
+
         #region Project Load
         public void OnProjectLoad()
         {
-            ///     `Database >> DataSet`
             if (!File.Exists(connection.FileName))
             {
                 CreateDatabase();
@@ -44,10 +106,18 @@ namespace NPC_Register
             else
             {
                 connection.Open();
-                // >Load Tables Here<
-                data.Tables.Add(ReturnTable("NPCs"));
+
+                #region Load Tables
+
                 data.Tables.Add(ReturnTable("ItemTemplates"));
+                data.Tables.Add(ReturnTable("CatalogueData"));
                 data.Tables.Add(ReturnTable("ItemInstances"));
+                data.Tables.Add(ReturnTable("CatalogueEntries"));
+                data.Tables.Add(ReturnTable("NPCs"));
+                data.Tables.Add(ReturnTable("Locations"));
+                data.Tables.Add(ReturnTable("HasAccessToCatalogue"));
+
+                #endregion
 
                 connection.Close();
             }
@@ -75,42 +145,25 @@ namespace NPC_Register
         #endregion
 
         #region Project Save
-        public void OnProjectSave() {
+        public void OnProjectSave()
+        {
             if (!File.Exists(connection.FileName))
             {
                 CreateDatabase();
-                
+                WriteToTables();
             }
-            else {
+            else
+            {
                 //Surely there's a way to update the database without rewriting it
                 //      :/
                 File.Delete(connection.FileName);
                 CreateDatabase();
 
-                var tblDictionary = GetDictionary();
-
-                connection.Open();
-
-                #region Write To Tables
-
-                // Write Order is >IMPORTANT< because of Table Dependencies
-                // Always check Foreign Keys before making changes here
-
-                WriteToItemTemplates(tblDictionary["ItemTemplates"]);
-                WriteToCatalogueData(tblDictionary["CatalogueData"]);
-                WriteToItemInstances(tblDictionary["ItemInstances"]);
-                WriteToCatalogueEntries(tblDictionary["CatalogueEntries"]);
-                WriteToLocations(tblDictionary["Locations"]);
-                WriteToNPCs(tblDictionary["NPCs"]);
-                WriteToHasAccessToCatalogue(tblDictionary["HasAccessToCatalogue"]);
-
-                #endregion
-
-                connection.Close();
+                WriteToTables();
             }
         }
 
-        #region Write To Tables
+        #region Write To Tables Functions
 
         void WriteToItemTemplates(DataTable table) {
             foreach (DataRow row in table.Rows) {
@@ -163,17 +216,27 @@ namespace NPC_Register
 
         #endregion
 
+        void WriteToTables() {
+
+            connection.Open();
+
+            // Write Order is >IMPORTANT< because of Table Dependencies
+            // Always check Foreign Keys before making changes here
+
+            WriteToItemTemplates(data.Tables[tableIndex["ItemTemplates"]]);
+            WriteToCatalogueData(data.Tables[tableIndex["CatalogueData"]]);
+            WriteToItemInstances(data.Tables[tableIndex["ItemInstances"]]);
+            WriteToCatalogueEntries(data.Tables[tableIndex["CatalogueEntries"]]);
+            WriteToLocations(data.Tables[tableIndex["Locations"]]);
+            WriteToNPCs(data.Tables[tableIndex["NPCs"]]);
+            WriteToHasAccessToCatalogue(data.Tables[tableIndex["HasAccessToCatalogue"]]);
+
+            connection.Close();
+        }
+
         private void SqliteNoQuery(string command) {
             cmd.CommandText = command;
             cmd.ExecuteNonQuery();
-        }
-
-        Dictionary<string, DataTable> GetDictionary() {
-            Dictionary<string, DataTable> dic = new Dictionary<string, DataTable>();
-            foreach (DataTable table in data.Tables) {
-                dic.Add(table.TableName, table);
-            }
-            return dic;
         }
 
         #endregion
